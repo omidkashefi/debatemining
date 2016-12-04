8a2879555a4fb189939f6b1ef4eef90de122b4a8
Omid Kashefi <omid.kashefi@gmail.com>
2016-12-04 17:42:02 -0500

    Update with ignoring wrong enteries

--- twitter/python/csv.py
+++ twitter/python/csv.py
@@ -3,6 +3,8 @@ import sys
 with open(sys.argv[1] + "all") as fa, open(sys.argv[1] + "sent") as fs:
     for la, ls in zip(fa, fs):
         parts = la.split(';')
+        if len(parts) < 2:
+            continue
         date = parts[0]
         text = parts[1]
 
