88b73e9a10b63e6cfdf858b9bca30d8e0c610372
Omid Kashefi <omid.kashefi@gmail.com>
2016-12-04 17:42:36 -0500

    unicode support and stdin

--- sentiment/s2v.py
+++ sentiment/s2v.py
@@ -1,4 +1,9 @@
 import sense2vec
+import fileinput
+
 model = sense2vec.load()
-freq, query_vector = model["debate|NOUN"]
-print model.most_similar(query_vector, n=3)
+print "Enter topic: "
+for line in fileinput.input():
+    freq, query_vector = model[u"{}|NOUN".format(line.strip())]
+    print model.most_similar(query_vector, n=10)
+    print "Enter topic: "
